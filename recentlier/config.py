deb68b86bd4309a7a80826d610a257ee40f890b5
kragebein <stian.langvann@gmail.com>
2020-02-08 06:52:27 +0100

    Moved callback string to config

--- recentlier/config.py
+++ recentlier/config.py
@@ -8,7 +8,7 @@ class conf():
         if not os.path.exists(self.file):
             print('{} not found, creating default config..'.format(self.file))
             self.config['PLAYLIST'] = {'name' : 'Recentlier Releaseder','size' :30}
-            self.config['APPLICATION'] = {'update_interval': 0, 'client_secret': '', 'client_id': ''}
+            self.config['APPLICATION'] = {'update_interval': 0, 'client_secret': '', 'client_id': '', 'callback': 'http://localhost'}
             with open(self.file, 'w') as configfile:
                 self.config.write(configfile)
         self.config.read(self.file)
@@ -19,6 +19,7 @@ class conf():
         self.loop = application['update_interval']
         self.cic = application['client_secret']
         self.cid = application['client_id']
+        self.callback = application['callback']
         if len(self.cid) < 1 or len(self.cic) < 1:
             print('ERROR. YOU MUST UPDATE "self.cid" and "self.cic" in bot/spotify.py before you continue!')
             sys.exit(1)
